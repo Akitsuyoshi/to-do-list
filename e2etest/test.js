6013a59d805084edda133c0b4e56a35859b5e1df
Akitsuyoshi <ka090824@gmail.com>
2017-07-24 21:55:36 +0800

    create UI

--- e2etest/test.js
+++ e2etest/test.js
@@ -1,3 +1,5 @@
+/* global describe, it, browser */
+
 const expect = require('chai').expect;
 
 describe('TodoList App', () => {
@@ -5,6 +7,16 @@ describe('TodoList App', () => {
     browser.url('http://localhost:3000/');
     const actualTitle = browser.getTitle();
 
-    expect(actualTitle).to.eql('Todo List');
+    expect(actualTitle).to.equal('Todo List');
+  });
+
+  it('should allow to create a Todo', () => {
+    const todoText = 'get better at testing';
+    browser.url('http://localhost:3000/');
+    browser.click('.todo-submit');
+
+    const actual = browser.element('.todo-text').getText();
+
+    expect(actual).to.equal(todoText);
   });
 });
